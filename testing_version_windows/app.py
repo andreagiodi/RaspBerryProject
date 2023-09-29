e4ca20bf55208965eafa0b2a964d44f0453d51bf
andreagiodi <90761154+andreagiodi@users.noreply.github.com>
2023-09-29 07:59:20 +0100

    Update app.py

--- testing_version_windows/app.py
+++ testing_version_windows/app.py
@@ -7,7 +7,6 @@ from PIL import Image
 from io import BytesIO
 import csv
 import uuid
-import mysql.connector
 import pandas as pd
 
 app = Flask(__name__)
@@ -177,15 +176,14 @@ def confirmreg():
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
 
-    count = 1
-    while count <= 30:  
-        faces = face_cascade.detectMultiScale(gray, 1.3, 4)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            face = gray[y:y + h, x:x + w]
-            face_resize = cv2.resize(face, (width, height))
-            cv2.imwrite(os.path.join(path, f'{count}.png'), face_resize)
-            count += 1
+    count = 1 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        cv2.imwrite(os.path.join(path, f'{count}.png'), face_resize)
+        
 
     #cv2.imshow('OpenCV', image)
     #cv2.waitKey(0)
